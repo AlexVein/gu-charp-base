91ff86313216cefe7bdfa0302ab937fcae7e0d96
AlexVein <alexblackwolf@yandex.ru>
2021-02-17 23:53:32 +0300

    Task 1 completed

---
+++ Lesson_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Lesson_3.Task_1;
+
+namespace Lesson_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task_1 = new PrintArray();
+
+            task_1.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
