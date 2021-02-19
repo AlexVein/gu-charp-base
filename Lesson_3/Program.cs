fe05075672a3300ed30ccb580b2fa6fdcb777322
AlexVein <alexblackwolf@yandex.ru>
2021-02-19 19:31:35 +0300

    Task 4 complete

--- Lesson_3/Program.cs
+++ Lesson_3/Program.cs
@@ -2,6 +2,7 @@
 using Lesson_3.Task_1;
 using Lesson_3.Task_2;
 using Lesson_3.Task_3;
+using Lesson_3.Task_4;
 
 namespace Lesson_3
 {
@@ -12,10 +13,12 @@ namespace Lesson_3
             var task_1 = new PrintArray();
             var task_2 = new PhoneBook();
             var task_3 = new InvertedString();
+            var task_4 = new SeaBattle();
 
             //task_1.Start();
             //task_2.Start();
-            task_3.Start();
+            //task_3.Start();
+            task_4.Start();
 
             Console.ReadLine();
         }
