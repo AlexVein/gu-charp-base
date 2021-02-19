5e743b06f784e9da1b329cb21536d827416b3f44
AlexVein <alexblackwolf@yandex.ru>
2021-02-19 14:34:21 +0300

    Task 2 complete

--- Lesson_3/Program.cs
+++ Lesson_3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Lesson_3.Task_1;
+using Lesson_3.Task_2;
 
 namespace Lesson_3
 {
@@ -8,8 +9,10 @@ namespace Lesson_3
         static void Main(string[] args)
         {
             var task_1 = new PrintArray();
+            var task_2 = new PhoneBook();
 
-            task_1.Start();
+            //task_1.Start();
+            task_2.Start();
 
             Console.ReadLine();
         }
---
+++ Lesson_3/Task_2/PhoneBook.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Написать программу — телефонный справочник — создать двумерный массив 5*2,
+ * хранящий список телефонных контактов: первый элемент хранит имя контакта,
+ * второй — номер телефона/e-mail.
+ */
+
+using System;
+
+namespace Lesson_3.Task_2
+{
+    public class PhoneBook
+    {
+        string[,] phoneBook =
+        {
+            {"Alex Dawson", "5693"}, 
+            {"Jude Rise", "5612"}, 
+            {"Mark Zukken", "6213"},
+            {"John Raily", "6136"}
+        };
+
+        public void Start()
+        {
+            Console.WriteLine("Name\t\tPhone\n---------------------");
+
+            for (int i = 0; i < phoneBook.GetLength(0); i++)
+            {
+                for (int j = 0; j < phoneBook.GetLength(1); j++)
+                {
+                    Console.Write($"{phoneBook[i, j]}\t");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
