bb472d7b05de79c6570c8e54f46671c0009f449f
AlexVein <alexblackwolf@yandex.ru>
2021-02-22 14:42:14 +0300

    Homework complete

--- Lesson_3/Program.cs
+++ Lesson_3/Program.cs
@@ -10,17 +10,50 @@ namespace Lesson_3
     {
         static void Main(string[] args)
         {
-            var task_1 = new PrintArray();
-            var task_2 = new PhoneBook();
-            var task_3 = new InvertedString();
-            var task_4 = new SeaBattle();
+            while (true)
+            {
+                Console.Clear();
 
-            //task_1.Start();
-            //task_2.Start();
-            //task_3.Start();
-            task_4.Start();
+                Console.WriteLine("1. Вывести элменты диагонали массива" +
+                                  "\n2. Телефонный справочник" +
+                                  "\n3. Инверсия строки" +
+                                  "\n4. \"Морской бой\" "+
+                                  "\n\n0. Выход");
 
-            Console.ReadLine();
+                Console.Write("\n\nВведите нужный пункт и нажмите Enter: ");
+                var select = Console.ReadLine();
+
+                if (select.Equals("0"))
+                    break;
+
+                Console.Clear();
+
+                switch (select)
+                {
+                    case "1":
+                        var task_1 = new PrintArray();
+                        task_1.Start();
+                        Console.ReadLine();
+                        break;
+                    case "2":
+                        var task_2 = new PhoneBook();
+                        task_2.Start();
+                        Console.ReadLine();
+                        break;
+                    case "3":
+                        var task_3 = new InvertedString();
+                        task_3.Start();
+                        Console.ReadLine();
+                        break;
+                    case "4":
+                        var task_4 = new SeaBattle();
+                        task_4.Start();
+                        Console.ReadLine();
+                        break;
+                    default:
+                        continue;
+                }
+            }
         }
     }
 }
