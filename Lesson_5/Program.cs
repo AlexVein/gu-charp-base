480f3a7680396cffa20d178d3e76c389a5c75aac
AlexVein <alexblackwolf@yandex.ru>
2021-03-03 11:59:13 +0300

    Task 1 completed

---
+++ Lesson_5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Lesson_5.Task_1;
+
+namespace Lesson_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+
+                Console.WriteLine("1. Сохранение в текстовый файл." +
+                                  "\n2. Запись времени при старте" +
+                                  "\n3. Запись в бинарный файл" +
+                                  "\n\n0. Выход");
+
+                Console.Write("\n\nВведите нужный пункт и нажмите Enter: ");
+                var select = Console.ReadLine();
+
+                if (select.Equals("0"))
+                    break;
+
+                Console.Clear();
+
+                switch (select)
+                {
+                    case "1":
+                        var task_1 = new SaveToTxt();
+                        task_1.Start();
+                        Console.ReadLine();
+                        break;
+                    case "2":
+                    case "3":
+                        break;
+                    default:
+                        continue;
+                }
+            }
+        }
+    }
+}
---
+++ Lesson_5/Task_1/SaveToTxt.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.
+ */
+
+using System;
+using System.IO;
+using System.Text;
+
+namespace Lesson_5.Task_1
+{
+    public class SaveToTxt
+
+    {
+        private string text = string.Empty;
+        private const string fileName = "Task_1.txt";
+
+        public void Start()
+        {
+            Console.Write("Введите текст: ");
+            text = Console.ReadLine();
+
+            File.WriteAllText(fileName, text, Encoding.UTF8);
+            Console.WriteLine($"Текст успешно записан в файл {fileName}");
+        }
+    }
+}
